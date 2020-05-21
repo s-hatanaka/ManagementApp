18857d23c54438a822d83902cd05df14221ad038
sari.hatanaka <s-hatanaka@i-enter.co.jp>
2020-05-21 16:25:27 +0900

    Initial Commit

---
+++ ManagementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManagementApp
+//
+//  Created by 畑中 彩里 on 2020/05/21.
+//  Copyright © 2020 sari.hatanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
